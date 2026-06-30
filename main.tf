ce0269d4db1bc499043c770eb9386a41ab4f80ed
Jakub Sokołowski <jakub@status.im>
2026-06-30 11:53:56 +0200

    all: bump nixpkgs, ansible, use gopass, optional pkgs, cache move
    Changes from infra-template:
    
    - cd0c9ba - nix: ugrade from Ansible 2.17 to 2.18
    - 2e0d34e - nix: bump nixpkgs input to nixos.25.11
    - 9d588ae - vault: cleanup cache dir based on file prefix
    - bb7e87b - vault: put cache in SECRETS_DIR by default
    - 339c9d3 - flake: support adding optional pkgs.nix
    - daefaa9 - flake: add pysocks python package for proxy support
    - 9b1c8d5 - switch from pass to gopass
    
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- main.tf
+++ main.tf
@@ -2,12 +2,12 @@
 
 terraform {
   backend "consul" {
-    address = "https://consul.statusim.net:8400"
+    address = "https://consul-api.infra.status.im:8400"
     /* Lock to avoid syncing issues */
     lock = true
     /* KV store has a limit of 512KB */
     gzip = true
     /* WARNING This needs to be changed for every repo. */
-    path      = "terraform/waku/"
+    path = "terraform/waku/"
   }
 }
